cbaec6f5d2e16fdc2dfe265e68a1571175017f1e
Danylo Shlapak <d.shlapak2314@gmail.com>
2025-09-24 23:58:07 +0300

    Initial commit

---
+++ src/test/java/edu/ukma/projectmanagementsystem/ProjectManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ukma.projectmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
