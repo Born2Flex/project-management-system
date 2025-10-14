03ce5834a4e033ebf31bf9053401ebc518b118eb
Danylo Shlapak <128870529+Born2Flex@users.noreply.github.com>
2025-10-14 17:49:46 +0200

    Merge SignIn and SignUp controllers into AuthController (#5)

--- src/main/java/edu/ukma/projectmanagementsystem/config/SwaggerConfig.java
+++ src/main/java/edu/ukma/projectmanagementsystem/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package edu.ukma.projectmanagementsystem.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,10 +25,14 @@ public class SwaggerConfig {
                 .type(SecurityScheme.Type.HTTP)
                 .scheme("bearer")
                 .bearerFormat("JWT");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securitySchemeName);
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Project Management System")
                         .description("API Description"))
+                .addSecurityItem(securityRequirement)
                 .components(new Components().addSecuritySchemes(securitySchemeName, securityScheme))
                 .servers(List.of(new Server().url(devServer)));
     }
