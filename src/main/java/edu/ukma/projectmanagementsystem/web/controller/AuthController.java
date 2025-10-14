70b42a82f75b5ccf9141dbf02a62bff1ef9ca7fd
Danylo Shlapak <d.shlapak2314@gmail.com>
2025-10-14 17:48:09 +0200

    Merge SignIn and SignUp controllers into AuthController

--- src/main/java/edu/ukma/projectmanagementsystem/web/controller/AuthController.java
+++ src/main/java/edu/ukma/projectmanagementsystem/web/controller/AuthController.java
@@ -1,5 +1,8 @@
 package edu.ukma.projectmanagementsystem.web.controller;
 
+import edu.ukma.projectmanagementsystem.service.business.UserService;
+import edu.ukma.projectmanagementsystem.service.dto.user.UserDto;
+import edu.ukma.projectmanagementsystem.service.dto.user.UserRegistrationDto;
 import edu.ukma.projectmanagementsystem.service.security.AuthService;
 import edu.ukma.projectmanagementsystem.service.security.dto.JwtResponseDto;
 import edu.ukma.projectmanagementsystem.service.security.dto.LoginDto;
@@ -8,22 +11,33 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Auth", description = "Auth management endpoints")
 @RestController
-@RequestMapping("/sign-in")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
-public class SignInController {
+public class AuthController {
 
+    private final UserService userService;
     private final AuthService authService;
 
-    @PostMapping("/login")
+    @PostMapping("/sign-up")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDto registerUser(@RequestBody UserRegistrationDto registrationDto) {
+        return userService.createUser(registrationDto);
+    }
+
+    @PostMapping("/sign-in")
     @Operation(summary = "Logic user using username and password")
     @ApiResponse(responseCode = "200",
             content = {@Content(schema = @Schema(implementation = JwtResponseDto.class), mediaType = "application/json")})
--- src/main/java/edu/ukma/projectmanagementsystem/web/controller/ProjectController.java
+++ src/main/java/edu/ukma/projectmanagementsystem/web/controller/ProjectController.java
@@ -17,6 +17,7 @@ import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -33,6 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Projects and Tasks", description = "Project and Task management endpoints")
 @RestController
 @RequestMapping("/projects")
 @RequiredArgsConstructor
