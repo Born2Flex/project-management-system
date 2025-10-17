56d046738bee17c90188f75f2a4875952984b344
DoctorKaufman <127844440+DoctorKaufman@users.noreply.github.com>
2025-10-18 00:08:05 +0300

    Removed TODO comments from ProjectManagementSystemApplication
    Removed TODO comments for user and task management.

--- src/main/java/edu/ukma/projectmanagementsystem/ProjectManagementSystemApplication.java
+++ src/main/java/edu/ukma/projectmanagementsystem/ProjectManagementSystemApplication.java
@@ -9,9 +9,4 @@ public class ProjectManagementSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectManagementSystemApplication.class, args);
     }
-
-    // TODO add users to the project
-    // TODO assign user to the task
-    // TODO update task dates
-
 }
